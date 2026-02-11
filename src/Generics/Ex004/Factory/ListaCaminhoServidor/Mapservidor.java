65e3a0197f023452744dae1af0552092f4c4ff18
João Carlos <jo029149@gmail.com>
2026-02-10 23:41:57 -0300

    Atualização do código
    Terminei a parte de criar o servidor e serviço, agora vou fazer a parte de entrar no servidor

--- src/Generics/Ex004/Factory/ListaCaminhoServidor/Mapservidor.java
+++ src/Generics/Ex004/Factory/ListaCaminhoServidor/Mapservidor.java
@@ -1,6 +1,5 @@
 package Generics.Ex004.Factory.ListaCaminhoServidor;
 import Generics.Ex004.Docs.GerarCaminho.Caminho;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,52 +11,76 @@ public class Mapservidor {
     private static Map<String, List<Caminho>> mapaServidores = new TreeMap<>();
 
     public static void criarServidor(String nomeServidor, String servico){
+
         if (mapaServidores.containsKey(nomeServidor)){
-            System.out.println("Este servidor está funcionando");
+            System.out.println("Este servidor "+nomeServidor+" está funcionando");
             List<Caminho> listaServico = mapaServidores.get(nomeServidor);
+
             for (Caminho servicos : listaServico){
+
                 if (servicos.getNomeServico().equalsIgnoreCase(servico)){
-                    System.out.println("Esse serviço já existe, tente criar outro diferente");
+                    System.out.println("Esse serviço "+servico+" já existe no servidor "+nomeServidor+", tente criar outro diferente");
                     return;
                 }
             }
 
-            List<Caminho> novoCaminho = new ArrayList<>();
-            novoCaminho.add(new Caminho(nomeServidor, servico));
-            mapaServidores.put(nomeServidor, novoCaminho);
+            listaServico.add(new Caminho(nomeServidor, servico));
+            Caminho caminho = new Caminho();
+            File caminhoServidor = new File(caminho.getLocalServidor(),nomeServidor);
+            File caminhoServico = new File(caminhoServidor,servico + ".txt");
+
+            if (!caminhoServidor.exists()){
+                if (caminhoServidor.mkdirs()){
+                    System.out.println("Este servidor tinha sido apagado e foi recriado");
+                }
+            }
+            try {
+                if (caminhoServico.createNewFile()){
+                    System.out.println("O serviço "+servico+" foi adicionado ao servidor "+nomeServidor);
+                }
+
+            }catch (IOException e){
+                System.out.println("O serviço "+servico+" não foi adicionado ao servidor "+nomeServidor);
+            }
         }
+
         else {
             System.out.println("Este servidor ainda não existe, vamos criar agora !");
             List<Caminho> novosServicos = new ArrayList<>();
             novosServicos.add(new Caminho(nomeServidor, servico));
             mapaServidores.putIfAbsent(nomeServidor, novosServicos);
 
+            Caminho caminho = new Caminho();
+            File servidor = new File(caminho.getLocalServidor(),nomeServidor);
 
-            File servidor = new File(nomeServidor);
+            if (!servidor.exists()){
 
-            boolean pasta = servidor.mkdirs();
-            if (pasta){
-                System.out.println("Pasta criada: "+ nomeServidor);
-            }
-            else {
-                System.out.println("Pasta não criada");
-            }
+                if (servidor.mkdirs()){
+                    System.out.println("Servidor criado com sucesso: "+servidor);
 
-            File servico = new File();
-
-            try {
-                if (path.createNewFile()){
-                    System.out.println("Arquivo criado: "+ path);
                 }
 
                 else {
-                    System.out.println("Arquivo não criado");
+                    System.out.println("Servidor não foi criado");
+                }
+            }
+
+            try {
+
+                File servidorServico = new File(servidor, servico+".txt");
+
+                if (servidorServico.createNewFile()){
+                    System.out.println("Serviço criado: "+ servidorServico);
+                }
+
+                else {
+                    System.out.println("Serviço não criado");
 
                 }
+
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-
         }
     }
 
