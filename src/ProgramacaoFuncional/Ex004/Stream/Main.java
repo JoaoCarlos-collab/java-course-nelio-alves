daff169536631cd1bf50546f73499ec2f69dd193
João Carlos <jo029149@gmail.com>
2026-02-16 17:03:12 -0300

    Atualizando

--- src/ProgramacaoFuncional/Ex004/Stream/Main.java
+++ src/ProgramacaoFuncional/Ex004/Stream/Main.java
@@ -1,4 +1,18 @@
 package ProgramacaoFuncional.Ex004.Stream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
+    public static void main (String[] args){
+        List<Integer> listaInteiros = Arrays.asList(1,2,3,4,5,6,7);
+        Stream <Integer> st1 = listaInteiros.stream();
+        Stream <Integer> st2 = listaInteiros.stream().map(x-> x * 10);
+        Stream <Integer> st3 = listaInteiros.stream().map(x-> x * 10).filter(x -> x >= 50);
+        int soma = listaInteiros.stream().mapToInt(x->x*20).sum();
+        System.out.println(Arrays.toString(st1.toArray()));
+        System.out.println(Arrays.toString(st2.toArray()));
+        System.out.println(Arrays.toString(st3.toArray()));
+        System.out.println(soma);
+    }
 }
